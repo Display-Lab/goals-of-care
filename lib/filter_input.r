0b7894aef6c53975120c78cd71b79b2270174a34
Colin Gross <grosscol@gmail.com>
2017-06-23 11:24:55 -0400

    Add component to generate first plot Add more discription to filter script comments.

--- lib/filter_input.r
+++ lib/filter_input.r
@@ -1,4 +1,4 @@
-library(tidyverse, warn.conflicts = FALSE)
+# Filter the input data to get only the rows that will be used susequently
 
 # If not given a file name, assume the input is input/clc.rdata
 options <- commandArgs(TRUE)
@@ -10,13 +10,15 @@ if(length(options)==0) {
 
 load(input_filename)
 
-# Filter input data
+# Create a array of boolean (TRUE) with one value per row in the data
 filter <- rep(TRUE, nrow(clc_summ))
 
 # Only use rows with the treatment specialty value of "All Speciialties"
+# Logical AND together the base filter with rows for which the condition is true
 filter <- filter & clc_summ[,"trt_spec"] == "All Specialties"
 
 # Apply filter to get filtered data
+# Subset the data by selecting rows that correspond to TRUE in the filter.
 flt_data <- clc_summ[filter,]
 
 # Save filtered data to intermediate directory (build) for later use & examination
