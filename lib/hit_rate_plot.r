b60b9e6d1b017af29e57860243b8d26956707b17
Colin Gross <grosscol@gmail.com>
2017-06-23 13:14:58 -0400

    Encapsulate plot generation in functions Uniquely name plot generation fuction and file   to be more descriptive than "plot_1"

---
+++ lib/hit_rate_plot.r
@@ -0,0 +1,58 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+
+# Generate the plot for a hit rate type performance metric
+# This script contains two utility functions:
+#   * Transform data for plotting convenience
+#   * Produce plot
+
+# Given data frame with "identifier", "hits" and "misses" columns,
+#  return data frame transformed for plotting 
+transform_to_hit_rate_data <- function(input_data){
+  # Convert multiple columns into two columns of key-value pairs
+  #  where the keys are the column titles and the values are those of the former columns
+  # For example, the row:
+  #
+  # location timepoint hits misses total_obs
+  # 556      2019 Q3   30   10     40 
+  # 
+  # Will become two rows after the gather operation with the parameters used in this script.
+  #
+  # location timepoint total_obs event      count
+  # 556      2015 Q1   40        misses     10
+  # 556      2015 Q1   40        hits       30
+  gathered <- gather(input_data, key="event", value="count", misses, hits) 
+  
+  # Convert key column (title of former columns) to a factor with the specified order of values
+  gathered$event = factor(gathered$event, levels = c("misses","hits"))
+  
+  return(gathered)
+}
+
+generate_hit_rate_plot <- function(plot_data){
+  plot.colors = c(total_obs="#000000", hits = "#4286f4", misses = "#e8e8e8")
+  
+  # Specify title of the plot
+  plot_title <- "How many newly admitted Veterans have a documented \ngoals of care conversation?" 
+  
+  # Specify the plot using grammar of graphics
+  hit_plot <- ggplot(plot_data, aes(x=timepoint, y = count, group=event)) +
+    geom_col(stat = "identity", aes(fill = event)) + 
+    geom_text(size = 4, aes(label = count), position = position_stack( vjust = 0.5))   +
+    geom_point(aes(y=total_obs,color="total_obs")) + 
+    geom_line( data = plot_data, aes(x = as.numeric(timepoint), y = total_obs, color = "total_obs")) +
+    labs(title=plot_title, x = " ", y = "Veterans admitted") +
+    scale_colour_manual(values = plot.colors, breaks = c("total_obs"), labels = c("Newly admitted \nveterans")) +
+    scale_fill_manual(values = plot.colors, breaks = c("hits","misses"), labels = c("Not documented", "Documented")) + 
+    theme(panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(),
+         panel.border = element_blank(),
+         panel.background = element_blank(),
+         legend.title = element_blank()
+         ) + 
+    guides(colour = guide_legend(order = 1))
+  
+  return(hit_plot)
+}
