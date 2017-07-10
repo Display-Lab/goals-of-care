e601431d676ab97b4a540a92f9c03994754845fc
Colin Gross <grosscol@gmail.com>
2017-07-10 13:24:04 -0400

    Read and check clc.csv instead of .rdata Specify expected column classes.

--- lib/check_input.r
+++ lib/check_input.r
@@ -5,12 +5,21 @@ options <- commandArgs(TRUE)
 
 # If not given a file name, assume the input is input/clc.rdata
 if(length(options)==0) {
-  input_filename <- file.path("input","clc.rdata")
+  input_filename <- file.path("input","clc.csv")
 }else{
   input_filename <- options[1]
 }
 
-load(input_filename)
+expected_input_colnames <- c("fy", "quart", "sta6a", "WardSID", "trtsp_1", "name", "WardLocationName", 
+                             "X_TYPE_", "X_FREQ_", "goc_7", "goc_14", "goc_30", "goc_pre90", 
+                             "goc_pre", "goc_none", "goc_post30", "goc_post")
+
+expected_input_classes <- c("integer", "integer", "character", "character", "character",  "character", "character",
+                            "integer", "integer", "integer", "integer", "integer", "integer",
+                            "integer", "integer", "integer", "integer")
+
+input_csv_file <- file.path("input","clc.csv")
+clc_summ <- read.csv( input_csv_file, header = TRUE, colClasses = expected_input_classes )
 
 # Expect data.frame named clc_summ
 cat("\nChecking name of data frame from input rdata file.\n")
@@ -22,11 +31,9 @@ if(exists("clc_summ") == FALSE || is.data.frame(clc_summ) == FALSE){
 }
 
 # Check that the header is as expected 
-expected_vars <- c("fy","quart","sta6a","WardSID","trtsp_1", "WardLocationName","_TYPE_","_FREQ_",
-  "goc_7","goc_14","goc_30","goc_pre90","goc_pre","goc_none","goc_post30","goc_post","trt_spec")
-expected_vars_count <- length(expected_vars)
+expected_vars_count <- length(expected_input_colnames)
 
-# Expect 17 variables
+# Check the number of columns matches the number of expected column names
 cat("Checking number of variables in data frame.\n")
 cat("Expected:", expected_vars_count, "encountered:", length(clc_summ),"\n")
 if(length(clc_summ) != expected_vars_count){
@@ -34,8 +41,8 @@ if(length(clc_summ) != expected_vars_count){
   quit(save="no", status=13)
 }
 
-# Check that names of columns matches expected
-col_differences <- setdiff(expected_vars, colnames(clc_summ))
+# Check that names of columns matches expected column names
+col_differences <- setdiff(expected_input_colnames, colnames(clc_summ))
 if(length(col_differences) == 0){
   cat("Found expected columns.\n")
 } else {
