f7121d29f7db5d2f8e30af46b50c662f54be87ef
Colin Gross <grosscol@gmail.com>
2017-07-10 14:27:14 -0400

    Use csv input instead of .rdata Update filter string constant to close issue #7

--- lib/filter_input.r
+++ lib/filter_input.r
@@ -3,19 +3,25 @@
 # If not given a file name, assume the input is input/clc.rdata
 options <- commandArgs(TRUE)
 if(length(options)==0) {
-  input_filename <- file.path("input","clc.rdata")
+  input_filename <- file.path("input","clc.csv")
 }else{
   input_filename <- options[1]
 }
 
-load(input_filename)
+input_classes <- c("integer", "integer", "character", "character", "character",  "character", "character",
+                            "integer", "integer", "integer", "integer", "integer", "integer",
+                            "integer", "integer", "integer", "integer")
+
+input_csv_file <- file.path("input","clc.csv")
+clc_summ <- read.csv( input_csv_file, header = TRUE, colClasses = input_classes )
+
 
 # Create a array of boolean (TRUE) with one value per row in the data
 filter <- rep(TRUE, nrow(clc_summ))
 
 # Only use rows with the treatment specialty value of "All Speciialties"
 # Logical AND together the base filter with rows for which the condition is true
-filter <- filter & clc_summ[,"trt_spec"] == "All Specialties"
+filter <- filter & clc_summ[,"trtsp_1"] == "All NH Treating Specialties"
 
 # Apply filter to get filtered data
 # Subset the data by selecting rows that correspond to TRUE in the filter.
