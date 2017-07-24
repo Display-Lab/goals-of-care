263970be6de66976c63fabb34f9736ad0b55c57f
Colin Gross <grosscol@gmail.com>
2017-07-24 15:48:49 -0400

    Allow check input to check hbpc input as well Allow either hbpc or clc input to be absent

--- lib/check_input.r
+++ lib/check_input.r
@@ -1,52 +1,79 @@
-# Pre-process check validity of rdata input file.
-
-# Get command line args entered by the user
-options <- commandArgs(TRUE)
-
-# If not given a file name, assume the input is input/clc.rdata
-if(length(options)==0) {
-  input_filename <- file.path("input","clc.csv")
-}else{
-  input_filename <- options[1]
+# Pre-process: check validity of input files.
+# Function to check that expected columns exist
+# Return boolean
+check_input <- function(input_path, expected_colnames, expected_classes){
+  summ <- read.csv( input_path, header = TRUE, colClasses = expected_classes )
+  
+  # Check that the header is as expected 
+  vars_count <- length(expected_colnames)
+ 
+  cat("Checking number of variables from: ", input_path, "\n")
+  cat("Expected:", vars_count, "encountered:", length(summ),"\n")
+  
+  if(length(summ) != vars_count){
+    cat("Unexpected number of variables!\n")
+    return(FALSE)
+  }
+  
+  # Check that names of columns matches expected column names
+  col_differences <- setdiff(expected_colnames, colnames(summ))
+  if(length(col_differences) == 0){
+    cat("Found expected columns.\n")
+  } else {
+    cat("Unexpected columns!\n")
+    print(col_differences)
+    return(FALSE)
+  }
+  
+  # return True if the input appears okay.
+  return(TRUE)
 }
 
-expected_input_colnames <- c("fy", "quart", "sta6a", "WardSID", "trtsp_1", "name", "WardLocationName", 
+# Check the CLC input if the input file exists
+expected_clc_colnames <- c("fy", "quart", "sta6a", "WardSID", "trtsp_1", "name", "WardLocationName", 
                              "X_TYPE_", "X_FREQ_", "goc_7", "goc_14", "goc_30", "goc_pre90", 
                              "goc_pre", "goc_none", "goc_post30", "goc_post")
 
-expected_input_classes <- c("integer", "integer", "character", "character", "character",  "character", "character",
+expected_clc_classes <- c("integer", "integer", "character", "character", "character",  "character", "character",
                             "integer", "integer", "integer", "integer", "integer", "integer",
                             "integer", "integer", "integer", "integer")
 
-input_csv_file <- file.path("input","clc.csv")
-clc_summ <- read.csv( input_csv_file, header = TRUE, colClasses = expected_input_classes )
+clc_filename <- file.path("input","clc.csv")
 
-# Expect data.frame named clc_summ
-cat("\nChecking name of data frame from input rdata file.\n")
-if(exists("clc_summ") == FALSE || is.data.frame(clc_summ) == FALSE){
-  cat("No data frame named clc_summ in rdata.\n")
-  quit(save="no", status=13)
-} else {
-  cat("Found expected data.frame\n")
+if(file.exists(clc_filename)){
+  clc_result <- check_input(clc_filename, expected_clc_colnames, expected_clc_classes)
 }
 
-# Check that the header is as expected 
-expected_vars_count <- length(expected_input_colnames)
+# Need to use make names as _TYPE and _FREQ_ aren't syntacticly valid
+expected_hbc_colnames <- make.names(c("fy","quart","hbpc_sta6a","_TYPE_","_FREQ_","no_hbpc","no_hbpc_ever","no_hbpc30",
+                           "hbpc","one_stop","numer1","denom1","numer2","denom2","numer3","denom3","denom90",
+                           "numer90","goc_pre","goc_any","goc_3","n1","n2","n3","min1","min2","min3",
+                           "mean1","mean2","mean3","max1","max2","maX3","goc_perc1","goc_perc2","goc_perc3","goc_any_perc"))
 
-# Check the number of columns matches the number of expected column names
-cat("Checking number of variables in data frame.\n")
-cat("Expected:", expected_vars_count, "encountered:", length(clc_summ),"\n")
-if(length(clc_summ) != expected_vars_count){
-  cat("Unexpected number of variables!\n")
-  quit(save="no", status=13)
+expected_hbc_classes <- c("integer","integer","factor",rep("character",5),
+                           "integer","character","integer","character","integer","character","integer","character","character",
+                           "integer","integer",rep("character",18) )
+
+hbc_filename <- file.path("input","hbpc.csv")
+if(file.exists(hbc_filename)){
+  hbc_result <- check_input(hbc_filename, expected_hbc_colnames, expected_hbc_classes)
 }
 
-# Check that names of columns matches expected column names
-col_differences <- setdiff(expected_input_colnames, colnames(clc_summ))
-if(length(col_differences) == 0){
-  cat("Found expected columns.\n")
-} else {
-  cat("Unexpected columns!\n")
-  print(col_differences)
-  quit(save="no", status=13)
+
+# Exit with status code 65 in the even hbpc or clc input files aren't good
+if(exists('clc_result')){
+  if(clc_result == FALSE){
+    quit(save='no', status=65)
+  }
+}
+if(exists('hbc_result')){
+  if(clc_result == FALSE){
+    quit(save='no', status=65)
+  }
+}
+
+# Handle case where neither input exist
+if(!exists('clc_result') && !exists('hbc_result')){
+  cat("\n\n!!! NO INPUT FILES FOUND !!!")
+  quit(save='no', status=65)
 }
