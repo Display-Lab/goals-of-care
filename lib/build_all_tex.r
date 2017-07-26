09de9fb60846523aac93826ba6b0798b5b62ae8f
Colin Gross <grosscol@gmail.com>
2017-07-26 19:39:07 -0400

    Make build script do both hbpc and clc. Rename report Rnw files for consistency. Rework naming scheme to append memo_type to id for ease of sorting.

--- lib/build_all_tex.r
+++ lib/build_all_tex.r
@@ -1,48 +1,73 @@
 library(knitr)
-# Create tex output for each plot
 
-# If not given a file name, assume the input is build/performance.rdata
-options <- commandArgs(TRUE)
-if(length(options)==0) {
-  input_filename <- file.path("build","performance.rdata")
-}else{
-  input_filename <- options[1]
+report_dir_path <- function (id, memo_type){
+  name <- file.path("build","reports",paste(id, memo_type, sep="_"))
 }
 
-load(input_filename)
+create_report_dir <- function(id, memo_type){
+  dir.create(report_dir_path(id, memo_type), showWarnings = FALSE)
+}
 
-generate_clc <- function(id){
-  
-  # Set the id for which the report will be generated
-  selected_id <- id
+# Generate a report for a single recipient
+# Id selects the memo recipient to subset the data
+# Use clc or hbpc as the memo type
+generate_report <- function(id, memo_type){
+  # Setup the directory and file names.
+  memo_dir_name <- paste(memo_type,"memo",sep="_")
+  report_filename <- paste(id, memo_type, "tex", sep=".")
+  dest_dir <- report_dir_path(id, memo_type)
   
   # Generate a CLC Report passing selected_id via the global env
-  knitr_file <- file.path("lib","clc_memo","report_1.Rnw")
+  # Selected id is a variable used in the report script.
+  selected_id <- id
+  knitr_file <- file.path("lib",memo_dir_name,"report.Rnw")
   knit(input= knitr_file)
   
   # Clean destination figures directory
-  dest_dir <- file.path("build","reports",id)
-  
   unlink(file.path(dest_dir, "figure"), recursive = TRUE)
   
-  # Move the products of that report to the build/reports directory
-  report_filename <- paste0("clc_", id, ".tex")
+  # Move the products of the report to the build/reports directory
   file.rename( from="figure",       to=file.path(dest_dir, "figure") )
-  file.rename( from="report_1.tex", to=file.path(dest_dir, report_filename) )
+  file.rename( from="report.tex", to=file.path(dest_dir, report_filename) )
   
   # Copy the memo class file into the report directory
-  file.copy(from=file.path("lib","clc_memo","texMemo.cls"), to=file.path(dest_dir,"texMemo.cls"))
+  file.copy(from=file.path("lib",memo_dir_name,"texMemo.cls"), to=file.path(dest_dir,"texMemo.cls"))
   
+  # Return true for successfully generating and moving files
   return(TRUE)
 }
 
-# Get identifiers each of which will get it's own report.
-ids = levels(hit_rate_data$location)
+run_report_for_all_ids <- function(input_path, memo_type){
+  load(input_path)
+  
+  # Get identifiers each of which will get it's own report.
+  ids = levels(rate_data$id)
+  
+  # create build directories if they don't exist
+  dir.create(file.path("build", "reports"), showWarnings = FALSE)
+  num_dirs_created <- lapply(ids, FUN=create_report_dir, memo_type=memo_type )
+  
+  # run report generation for each id
+  successes <- sapply(ids, FUN=generate_report, memo_type=memo_type)
+  return(sum(successes))
+}
 
-# create build directories if they don't exist
-dir.create(file.path("build", "reports"), showWarnings = FALSE)
-num_dirs_created <- lapply(ids, FUN=function(x){ dir.create(file.path("build","reports",x), showWarnings = FALSE)} )
+clc_input <- file.path("build","performance_clc.rdata")
+hbpc_input <- file.path("build","performance_hbpc.rdata")
 
-# run report generation for each id
-num_reports_created <- lapply(ids, FUN=generate_clc)
+if(file.exists(clc_input)){
+  cat("Generating CLC Reports\n")
+  num_reports <- run_report_for_all_ids(clc_input, "clc")
+  cat("Created ", num_reports, " CLC reports.")
+}
+
+if(file.exists(hbpc_input)){
+  cat("Generating HBPC Reports\n")
+  num_reports <- run_report_for_all_ids(hbpc_input, "hbpc")
+  cat("Created ", num_reports, " HBPC reports.")
+}
+
+
+
+
 
