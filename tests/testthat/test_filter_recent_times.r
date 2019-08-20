ba9d6f98cfd0e7353faa5843c3485cf3dd748696
Colin Gross <grosscol@gmail.com>
2019-08-20 09:24:21 -0400

    Update test to reflect actual input col names.

--- tests/testthat/test_filter_recent_times.r
+++ tests/testthat/test_filter_recent_times.r
@@ -1,26 +1,27 @@
 library(tibble)
+library(lubridate)
 
 context('Test Time Filtering')
 
 input_table <- tibble::tribble(
-  ~fy, ~quart, ~sta6a, ~type,  ~value,
-  2018, 1,     "foo",  "short", 10,
-  2018, 1,     "foo",  "long",  10,
-  2018, 2,     "foo",  "short", 10,
-  2018, 2,     "foo",  "long",  10,
-  2018, 3,     "foo",  "short", 10,
-  2018, 3,     "foo",  "long",  10,
-  2018, 4,     "foo",  "short", 10,
-  2018, 4,     "foo",  "long",  10,
-  2017, 3,     "foo",  "short", 10,
-  2017, 3,     "foo",  "long",  10,
-  2017, 4,     "foo",  "short", 10,
-  2017, 4,     "foo",  "long",  10
+  ~report_month, ~sta6a, ~type,  ~value,
+  ymd("2017-03-01"),     "foo",  "short", 10,
+  ymd("2017-03-01"),     "foo",  "long",  10,
+  ymd("2017-04-01"),     "foo",  "short", 10,
+  ymd("2017-04-01"),     "foo",  "long",  10,
+  ymd("2018-01-01"),     "foo",  "short", 10,
+  ymd("2018-01-01"),     "foo",  "long",  10,
+  ymd("2018-02-01"),     "foo",  "short", 10,
+  ymd("2018-02-01"),     "foo",  "long",  10,
+  ymd("2018-03-01"),     "foo",  "short", 10,
+  ymd("2018-03-01"),     "foo",  "long",  10,
+  ymd("2018-04-01"),     "foo",  "short", 10,
+  ymd("2018-04-01"),     "foo",  "long",  10
 )
 
 test_that("Timepoints before most recent x are removed", {
   result <- filter_recent_times(input_table, 4) 
-  expect_equal(sum(result$fy == 2017), 0)
+  expect_equal(sum(result$report_month < ymd("2018-01-01")), 0)
 })
 
 test_that("All timepoints after most recent x are retained", {
