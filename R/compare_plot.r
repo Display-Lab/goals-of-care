f2efbf40ac655450563ef085aeda3ea9257a8bcd
Colin Gross <grosscol@gmail.com>
2019-11-26 20:21:47 -0500

    Include subtitle in comparision figure. Ameliorate squashed effect.

--- R/compare_plot.r
+++ R/compare_plot.r
@@ -1,6 +1,6 @@
 #' @title Compare Plot
 #' @description Create a rate plot figure.
-#' @param plot_data rate plot dataframe
+#' @param plot_data rate plot dataframe with avg and rate calculated
 #' @param plot_title string for plot title
 #' @param plot_subtitle string for plot subtitle. may be omitted?
 #' @param y_label string for y-axis label
@@ -8,12 +8,12 @@
 #' @importFrom scales pretty_breaks percent
 #' @export
 compare_plot <- function(plot_data, plot_title = "", plot_subtitle="", y_label = ""){
-  line_label <- "VA Median LST Completion"
+  line_label <- "National Average LST Completion"
   # Manually selected colors from Viridis Palette
   plot_colors = c("#90EE90", "#414487FF",  "#2A788EFF", "#22A884FF",  "#7AD151FF", "#FDE725FF")
   
   # Check for extra columns to be used as faceting and color fill factors:
-  extra_colnames <- quant_extra_colnames(names(plot_data))
+  extra_colnames <- comp_extra_colnames(names(plot_data))
   extra_color_vals <- c()
   if(length(extra_colnames) > 0){
     extra_color_vals <- unique(plot_data[,extra_colnames])
@@ -26,14 +26,16 @@ compare_plot <- function(plot_data, plot_title = "", plot_subtitle="", y_label =
   
   # Invert count_label. NA count labels become "no admits"
   zero_denoms <- plot_data$denominator == 0
+  zero_nums <- plot_data$count == 0
   plot_data$count_label <- NA
+  plot_data$count_label[zero_nums] <- "0%"
   plot_data$count_label[zero_denoms] <- "no admits"
   
-  g <- ggplot(plot_data, aes(x = timepoint, y = rate, group = event)) +
-    geom_col(aes(fill=event)) +
-    geom_line(aes(y=med, colour=line_label)) +
+  g <- ggplot(plot_data, aes(x = timepoint, y = rate)) +
+    geom_col(fill=plot_colors[3]) +
+    geom_line(aes(y=avg, colour=line_label), size=2 ) +
     geom_text(aes(y=.35, label=count_label), angle=90) +
-    labs(title = plot_title,  x = "", y = "") +
+    labs(title = plot_title,  x = "", y = "", subtitle = plot_subtitle) +
     scale_y_continuous(breaks=pretty_breaks(), labels=scales::percent, limits=c(0,ulim)) +
     scale_x_date(date_labels = "%Y %b") +
     scale_fill_manual( values=plot_colors,  breaks=names(plot_colors) )+
@@ -44,7 +46,9 @@ compare_plot <- function(plot_data, plot_title = "", plot_subtitle="", y_label =
       panel.background = element_blank(),
       legend.title = element_blank(),
       legend.position = "top",
-      axis.text.x = element_text(angle = 45, hjust = 1)) +
+      legend.margin=margin(c(1.5,1.5,1.5,1.5), unit="pt"),
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      plot.margin=unit(c(1,1,1,0), "pt")) +
     guides(colour = guide_legend(order = 1)) 
   # Add facet wrap if faceting columns are avaialble
   if(length(extra_colnames) > 0) {
@@ -53,9 +57,10 @@ compare_plot <- function(plot_data, plot_title = "", plot_subtitle="", y_label =
   return(g)
 }
 
-#' @description set diff of column names in plot data with expected names
-#' @describeIn compare_plot discover extra column names in plot data
-quant_extra_colnames <- function(cnames){
-  expected_names <- c('id', 'timepoint', 'event', 'count', 'limit', 'count_label', 'denominator', 'uquant', 'lquant', 'rate', 'mean', 'med')
+#' @title Comparison Extra Column Names
+#' @describeIn compare_plot discover extra column names in plot data for comparison plot
+comp_extra_colnames <- function(cnames){
+  expected_names <- c('id', 'timepoint', 'event', 'count', 'limit', 'count_label', 'denominator', 
+                      'uquant', 'lquant', 'rate', 'mean', 'avg')
   setdiff(cnames, expected_names)
 }
